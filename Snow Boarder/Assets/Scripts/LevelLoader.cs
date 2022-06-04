b284350e45b87255b954af3ba355518daec4b909
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-06-04 17:56:22 +0530

    Add Loading Screen and Loading Bar

--- Snow Boarder/Assets/Scripts/LevelLoader.cs
+++ Snow Boarder/Assets/Scripts/LevelLoader.cs
@@ -1,10 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelLoader : MonoBehaviour
 {
+    public Slider loadingSlider;
+    public GameObject loadingScreen;
+    public Text loadingBarText;
+
     public void LoadLevel(int sceneIndex)
     {
         StartCoroutine(LoadAsynchronously(sceneIndex));
@@ -14,9 +20,16 @@ public class LevelLoader : MonoBehaviour
     {
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
 
+        loadingScreen.SetActive(true);
+
+
         while (!operation.isDone)
         {
-            Debug.Log(operation.progress);
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            Debug.Log(progress);
+
+            loadingSlider.value = progress;
+            loadingBarText.text = (progress * 100f + "%");
 
             yield return null;
         }
