0b4acac2b38952f5f720f76c736a406c372e6709
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-06-04 16:14:51 +0530

    Add snow dust particle effect

---
+++ Snow Boarder/Assets/Scripts/DustTrail.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DustTrail : MonoBehaviour
+{
+
+    [SerializeField] private ParticleSystem snowEffect;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Ground"))
+        {
+            snowEffect.Play();
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            snowEffect.Stop();
+        }
+    }
+    
+    
+} // Class
--- Snow Boarder/Assets/Scripts/PlayerController.cs
+++ Snow Boarder/Assets/Scripts/PlayerController.cs
@@ -6,31 +6,51 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private GameObject player;
 
-    private Rigidbody2D rigid2D;
+    private Rigidbody2D _rigid2D;
 
     [SerializeField] private float torqueMagnitude;
+    [SerializeField] private float highSpeed;
+
+    private SurfaceEffector2D _sf2d;
 
     // Start is called before the first frame update
     void Start()
     {
-        rigid2D = GetComponent<Rigidbody2D>();
+        _rigid2D = GetComponent<Rigidbody2D>();
+        _sf2d = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         AddTorqueInput();
+        RespondToBoost();
+    }
+
+    private void RespondToBoost()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            _sf2d.speed = highSpeed;
+            
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            _sf2d.speed = 23f;
+        }
     }
 
     void AddTorqueInput()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            rigid2D.AddTorque(Time.deltaTime * torqueMagnitude);
+            _rigid2D.AddTorque(Time.deltaTime * torqueMagnitude);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            rigid2D.AddTorque((-1) * Time.deltaTime * torqueMagnitude);
+            _rigid2D.AddTorque((-1) * Time.deltaTime * torqueMagnitude);
         }
     }
+    
+    
 } // Class
