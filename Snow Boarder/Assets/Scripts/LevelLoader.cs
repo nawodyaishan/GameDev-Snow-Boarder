f95abc8fe9fd81379eac98d2cd3137908a87200b
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-06-04 17:29:42 +0530

    Add Loading Async Scene Index Coroutine

--- Snow Boarder/Assets/Scripts/LevelLoader.cs
+++ Snow Boarder/Assets/Scripts/LevelLoader.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public void LoadLevel(int sceneIndex)
     {
-        
+        StartCoroutine(LoadAsynchronously(sceneIndex));
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator LoadAsynchronously(int sceneIndex)
     {
-        
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!operation.isDone)
+        {
+            Debug.Log(operation.progress);
+
+            yield return null;
+
+        }
     }
+    
 }
